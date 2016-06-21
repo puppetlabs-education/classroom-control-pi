c241f80c82c129df68be56c7133d7ecc60e7692d
WilliamMiteff <wmmiteff@gmail.com>
2016-06-21 10:50:00 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -26,6 +26,10 @@ node default {
   # Example:
   #   class { 'my_class': }
   
+  unless $environment in [ 'production', 'staging' ] {
+    notify { "Warning: this is a development environment on ${::fqdn}": }
+  }
+  
   # example code for the classroom
   include examples::puppetize
   
