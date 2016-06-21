c8a47525b588ea4cb5d10e84106fb56336e5e96b
wlnewel <lee.newell@wal-mart.com>
2016-06-21 15:36:19 -0500

    Update windows.pp

--- site/profile/manifests/tuning/windows.pp
+++ site/profile/manifests/tuning/windows.pp
@@ -4,16 +4,14 @@ class profile::tuning::windows {
       value  => 'KeepAliveInterval',
       data   => '1',
     }
-
-  # Add more registry resources as directed
     registry::value { 'TcpMaxDataRetransmission':
-    key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
-    value => 'TcpMaxDataRetransmissions',
-    data => '5',
+      key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
+      value => 'TcpMaxDataRetransmissions',
+      data => '5',
     }
     registry::value { 'TcpTimedWaitDelay':
-    key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
-    value => 'TcpTimedWaitDelay',
-    data => '0x0000001e',
+      key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
+      value => 'TcpTimedWaitDelay',
+      data => '0x0000001e',
   }
 }
