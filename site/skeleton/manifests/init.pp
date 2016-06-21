e8f7ffa4948c66f08a6e632ce0cb30f380aafd93
wlnewel <lee.newell@wal-mart.com>
2016-06-21 13:41:45 -0500

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,6 +1,6 @@
 class skeleton {
   file { '/etc/skel':
-#    ensure => ???,   # what value should go here?
+    ensure => directory,
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
