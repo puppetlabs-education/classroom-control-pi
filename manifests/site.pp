7deefd4a72654ce7d09c2ba3ce2b914e2f5784af
jlharr <jlharr@hotmail.com>
2016-06-21 10:50:25 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,6 +28,6 @@ node default {
   
   # example code for the classroom
   include examples::puppetize
-  
-  notify { "Hello world! I am ${::fqdn}": }
+unless $environment in ['production','staging']{
+  notify { "Warning: this is a development envrionment on ${::fqdn}": }
 }
