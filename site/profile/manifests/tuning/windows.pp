e298986eca30210eeff080d3b8573ec1b3131510
wlnewel <lee.newell@wal-mart.com>
2016-06-21 15:41:12 -0500

    Update windows.pp

--- site/profile/manifests/tuning/windows.pp
+++ site/profile/manifests/tuning/windows.pp
@@ -4,14 +4,5 @@ class profile::tuning::windows {
       value  => 'KeepAliveInterval',
       data   => '1',
     }
-    registry::value { 'TcpMaxDataRetransmission':
-      key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
-      value => 'TcpMaxDataRetransmissions',
-      data => '5',
-    }
-    registry::value { 'TcpTimedWaitDelay':
-      key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
-      value => 'TcpTimedWaitDelay',
-      data => '0x0000001e',
-  }
+
 }
