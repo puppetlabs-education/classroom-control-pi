f024e45c577284e76496013eb003107d28aed375
GHB0794521 <bizzobb@gmail.com>
2016-06-22 13:41:40 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -29,5 +29,5 @@ node default {
   # example code for the classroom
   include examples::puppetize
   
-  notify { "Hello world! I am ${::fqdn}": }
+  notify { "The primary disk is ${::disks['sda']['size']} in size.": }
 }
