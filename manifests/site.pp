23693cadbddebee735f4e7b80ce8f2b814c7b7b7
pcuenco <pcuenco@convokesystems.com>
2017-06-13 16:42:35 -0400

    Create site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -21,6 +21,10 @@
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+node pcuenco.puppetlabs.vm {
+   notify { "LINUX ONLY" : }
+}
+
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
@@ -30,8 +34,4 @@ node default {
   include examples::puppetize
   
   notify { "This is the default message from the production environment": }
-}
-
-node pcuenco.puppetlabs.vm {
-   notify { "This will only be enforced on Linux node.":}
 }
